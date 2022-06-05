09c44049bf716766bf0727a3a03211ac0b14167e
ilhamyusuf27 <ilham.0992@students.amikom.ac.id>
2022-06-06 01:53:53 +0700

    update

--- soal2.js
+++ soal2.js
@@ -1,47 +1,30 @@
-const nama = [
-	'Abigail',
-	'Alexandra',
-	'Alison',
-	'Amanda',
-	'Angela',
-	'Bella',
-	'Carol',
-	'Caroline',
-	'Carolyn',
-	'Deirdre',
-	'Diana',
-	'Elizabeth',
-	'Ella',
-	'Faith',
-	'Olivia',
-	'Penelope',
-];
+const nama = ['Abigail', 'Alexandra', 'Alison', 'Amanda', 'Angela', 'Bella', 'Carol', 'Caroline', 'Carolyn', 'Deirdre', 'Diana', 'Elizabeth', 'Ella', 'Faith', 'Olivia', 'Penelope'];
 
 const getIndex = (keyWords) => {
 	const keys = [];
-	const lower = nama
-		.map((element) => element.toLowerCase())
-		.filter((element, i) => {
-			if (element.includes(keyWords.toLowerCase())) keys.push(i);
-		});
+	const toLower = nama.map((e) => e.toLowerCase());
+	toLower.filter((e, i) => {
+		if (e.includes(keyWords.toLowerCase())) keys.push(i);
+	});
 	return keys;
 };
 
 const searchName = (keyWords, amount, callback) => {
-	const hasil = [];
-
 	if (typeof keyWords === 'string') {
-		const index = callback(keyWords);
+		const temp = [];
+		const getIndex = callback(keyWords);
 
 		for (let i = 0; i < amount; i++) {
-			if (nama[index[i]] !== undefined) hasil.push(nama[index[i]]);
+			if (nama[getIndex[i]] !== undefined) temp.push(nama[getIndex[i]]);
 		}
-		console.log(hasil);
-		if (amount > index.length)
-			console.log(`Hasil yang ditemukan hanya ${index.length} nama saja!`);
+		console.log(temp);
+
+		if (amount > getIndex.length) console.log(`Hasil yang ditemukan hanya ${getIndex.length} nama saja!`);
 	} else {
 		console.log(`Keyword harus string`);
 	}
 };
 
-searchName('an', 5, getIndex);
+// console.log(getIndex('an'));
+
+searchName('an', 3, getIndex);
--- soal3.js
+++ soal3.js
@@ -1,9 +1,5 @@
 const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
-	if (
-		typeof nilaiAwal === 'number' &&
-		typeof nilaiAkhir === 'number' &&
-		Array.isArray(dataArray) === true
-	) {
+	if (typeof nilaiAwal === 'number' && typeof nilaiAkhir === 'number' && Array.isArray(dataArray) === true) {
 		if (nilaiAwal < nilaiAkhir && dataArray.length > 5) {
 			const number = [];
 			for (let i = 0; i < dataArray.length; i++) {
@@ -11,21 +7,20 @@ const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
 					number.push(dataArray[i]);
 				}
 			}
-			if (number.length === 0) console.log('nilai tidak ditemukan');
-			console.log(number.sort((a, b) => a - b));
+			if (number.length === 0) {
+				console.log('nilai tidak ditemukan');
+			} else {
+				console.log(number.sort((a, b) => a - b));
+			}
 		} else if (nilaiAwal > nilaiAkhir) {
 			console.log('Nilai akhir harus lebih besar dari nilai awal');
-		} else if (dataArray.length < 5) {
+		} else if (dataArray.length <= 5) {
 			console.log('Jumlah angka dalam dataArray harus lebih dari 5');
 		} else {
-			console.log(
-				`nilaiAwal harus lebih kecil dari nilaiAkhir dan dataArray harus lebih dari 5`,
-			);
+			console.log(`nilaiAwal harus lebih kecil dari nilaiAkhir dan dataArray harus lebih dari 5`);
 		}
 	} else {
-		console.log(
-			'Tipe data nilaiAwal, nilaiAkhir harus number, parameter dataArray harus array!',
-		);
+		console.log('Tipe data nilaiAwal, nilaiAkhir harus number, parameter dataArray harus array!');
 	}
 };
 
